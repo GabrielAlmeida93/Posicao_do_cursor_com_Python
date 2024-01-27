369d448f008e0f04f977d099565cd9d15c881d4b
Gabriel Almeida <146780252+GabrielAlmeida93@users.noreply.github.com>
2024-01-27 17:16:58 -0300

    Create posicao_do_mouse.py

---
+++ posicao_do_mouse.py
@@ -0,0 +1,5 @@
+'''importar bibliotecas pyautogui e time'''
+import pyautogui
+import time
+time.sleep(5) #tempo para levar o cursor até o ponto desejado antes de execultar o comando de leitura
+print(pyautogui.position()) #comando de leitura da posição
